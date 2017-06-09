3d1770a638b9cad433e5a280ef3eeefe7d69f2f6
zhangqian <zq_674360351@163.com>
2017-06-09 10:16:20 +0800

    初始化项目

---
+++ public/js/login.js
@@ -0,0 +1,21 @@
+define(['jquery','cookie'],function($){
+	//实现登录
+	$('#loginId').click(function () {
+		$.ajax({
+			type : 'post',
+			url : '/api/login',
+			data : $('#loginForm').serialize(),
+			dataType : 'json',
+			success : function(data){
+				if(data.code == 200) {
+					//把登录的用户信息存储到cookie里面，方便页面之间进行数据共享
+					$.cookie('loginInfo',JSON.stringify(data.result),{path:'/'});
+					//跳转到主页面
+					location.href = '/index/index';
+				}
+			}
+		});
+		//阻止默认事件
+		return false;
+	});
+});
