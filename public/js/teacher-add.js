96bbd5c153bde7826276ff20fe944d3f55ff8bd7
zhangqian <zq_674360351@163.com>
2017-06-10 22:26:32 +0800

    实现添加和编辑讲师-半成品

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,3 +1,40 @@
-define(['jquery'],function($){
+define(['jquery','util','template'],function($,util,template){
+	//设置导航选中
+	util.setMenu('/teacher/list');
+	// 提交表单
+	$('#addTeacherBtn').click(function(){
+		$.ajax({
+			type : 'post',
+			url : '/api/teacher/add',
+			data : $('#addTeacherForm').serialize(),
+			dataType : 'json',
+			success : function(data){
+				console.log(data);
+			}
+		});
+	});
+	// 获取参数中的tc_id
+	var tcId = util.qs('tc_id',location.search);
+	if(tcId){
+		// 编辑讲师
+		// 根据id查询数据
+		$.ajax({
+			type : 'get',
+			url : '/api/teacher/edit',
+			data : {tc_id : tcId},
+			dataType : 'json',
+			success : function(data){
+				$('#navFlag').html('讲师编辑');
+				data.result.operateFlag = '编 辑';
+				var html = template('teacherFormTpl',data.result);
+				$('#teacherFormInfo').html(html);
+			}
+		});
+	}else {
+		//添加讲师
+		$('#navFlag').html('讲师添加');
+		var html = template('teacherFormTpl',{operateFlag : '添 加'});
+		$('#teacherFormInfo').html(html);
+	}
 	
 });
