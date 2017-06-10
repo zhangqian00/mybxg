c3d14d3e0a4f458b3fc07e8af86568abb5a11657
zhangqian <zq_674360351@163.com>
2017-06-10 20:33:17 +0800

    修改头像和用户名bug

--- public/js/common.js
+++ public/js/common.js
@@ -24,7 +24,7 @@ define(['jquery', 'template', 'nprogress', 'cookie'], function($, template,nprog
     //获取登录用户的cookie信息
     var loginInfo = $.cookie('loginInfo') && JSON.parse($.cookie('loginInfo'));
     if (loginInfo) {
-        var loginTpl = '<div calss="avatar img-circle"><img src="{{tc_avatar}}"></div><h4>{{tc_name}}</h4>';
+        var loginTpl = '<div class="avatar img-circle"><img src="{{tc_avatar}}"></div><h4>{{tc_name}}</h4>';
         var html = template.render(loginTpl,loginInfo); 
         $('#loginInfoTpl').html(html);
         //渲染页面
--- public/js/util.js
+++ public/js/util.js
@@ -1,4 +1,4 @@
-define(['jquery'],function(){
+define(['jquery'],function($){
 	return {
 		setMenu : function(pathname){
 			$('.aside .navs a').removeClass('active');
