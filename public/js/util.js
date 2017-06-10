5b2b9ec6930ad68cfb805b6db1bb8f1142aa7b44
zhangqian <zq_674360351@163.com>
2017-06-10 11:07:55 +0800

    实现根据id查询值的功能

--- public/js/util.js
+++ public/js/util.js
@@ -3,6 +3,21 @@ define(['jquery'],function(){
 		setMenu : function(pathname){
 			$('.aside .navs a').removeClass('active');
 			$('.aside .navs a[href="'+pathname+'"]').addClass('active');
+		},
+		qs : function(attr,param){
+			// ?abc=123&flag=456
+			var p = param.substring(1);
+			var arr = p.split('&');
+			var retValue = '';
+			arr.forEach(function(element,index){
+				var kv = element.split('=');
+				if(attr == kv[0]) {
+					retValue = kv[1];
+					// return的作用是结束循环
+					return;
+				}
+			});
+			return retValue;
 		}
 	}
 });
