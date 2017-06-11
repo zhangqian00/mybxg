8d4554a2acf0dc519a330261602137444499e572
zhangqian <zq_674360351@163.com>
2017-06-11 11:10:03 +0800

    修改性别显示bug-完成添加和编辑讲师功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,20 +1,27 @@
 define(['jquery','util','template'],function($,util,template){
 	//设置导航选中
 	util.setMenu('/teacher/list');
-	// 提交表单
-	$('#addTeacherBtn').click(function(){
-		$.ajax({
-			type : 'post',
-			url : '/api/teacher/add',
-			data : $('#addTeacherForm').serialize(),
-			dataType : 'json',
-			success : function(data){
-				console.log(data);
-			}
-		});
-	});
 	// 获取参数中的tc_id
 	var tcId = util.qs('tc_id',location.search);
+
+	// 提交表单处理
+	function submitForm(url){
+		$('#addTeacherBtn').click(function(){
+			$.ajax({
+				type : 'post',
+				url : url,
+				data : $('#addTeacherForm').serialize(),
+				dataType : 'json',
+				success : function(data){
+					if(data.code == 200) {
+						location.href = '/teacher/list';
+					}
+				}
+			});
+		});
+	}
+	
+	
 	if(tcId){
 		// 编辑讲师
 		// 根据id查询数据
@@ -28,13 +35,16 @@ define(['jquery','util','template'],function($,util,template){
 				data.result.operateFlag = '编 辑';
 				var html = template('teacherFormTpl',data.result);
 				$('#teacherFormInfo').html(html);
+				// 编辑提交
+				submitForm('/api/teacher/update');									
 			}
 		});
 	}else {
 		//添加讲师
 		$('#navFlag').html('讲师添加');
-		var html = template('teacherFormTpl',{operateFlag : '添 加'});
+		var html = template('teacherFormTpl',{operateFlag:'添 加',tc_gender:0});
 		$('#teacherFormInfo').html(html);
-	}
-	
+		// 添加提交
+		submitForm('/api/teacher/add');
+	}	
 });
