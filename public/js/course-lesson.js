2432f13557bf9145fdcad5c548d65e5c570bf9ab
zhangqian <zq_674360351@163.com>
2017-06-15 20:28:34 +0800

    课时编辑-显示信息

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','bootstrap'],function($,template,util){
 	util.setMenu('/course/add');
 
 	// 获取课程id
@@ -12,6 +12,21 @@ define(['jquery','template','util'],function($,template,util){
 		success : function(data){
 			var html = template('lessonTpl',data.result);
 			$('#lessonInfo').html(html);
+			// 实现课时预览功能
+			$('.lessonedit').click(function(){
+				var ctId = $(this).attr('data-ctId');
+				$.ajax({
+					type : 'get',
+					url : '/api/course/chapter/edit',
+					data : {ct_id : ctId},
+					datType : 'json',
+					success : function(data){
+						var html = template('lessonModalTpl',data.result);
+						$('#lessonModalInfo').html(html);
+						$('#chapterModal').modal();
+					}
+				});
+			});
 		}
 	});
 });
