615a5f0ea33eee846d431ebc186b49909b03509d
zhangqian <zq_674360351@163.com>
2017-06-15 20:08:23 +0800

    课时管理显示信息

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,3 +1,17 @@
 define(['jquery','template','util'],function($,template,util){
-	
+	util.setMenu('/course/add');
+
+	// 获取课程id
+	var csId = util.qs('cs_id',location.search);
+	// 根据课程id查询课时信息
+	$.ajax({
+		type : 'get',
+		url : '/api/course/lesson',
+		data : {cs_id : csId},
+		dataType : 'json',
+		success : function(data){
+			var html = template('lessonTpl',data.result);
+			$('#lessonInfo').html(html);
+		}
+	});
 });
